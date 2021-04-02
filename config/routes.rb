aa5284343dbe697e182e931ae4c5e29c0df48101
gai965 <kotodama51080@gmail.com>
2021-04-03 03:28:44 +0900

    ゲストユーザログインの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'mains#index'
   resources :mains, only: :index
+
+  # ゲストユーザログイン用のルーティング
+  post '/mains/guest_sign_in', to: 'mains#guest_sign_in'
 end
