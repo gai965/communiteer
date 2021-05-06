520c2b2afb19e6326feda0264fb4c28810a3b615
gai965 <kotodama51080@gmail.com>
2021-05-06 11:03:07 +0900

    「応援する」の解除

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,16 +1,23 @@
 class CheersController < ApplicationController
  before_action :move_to_index, only: [:volunteer_cheer_index, :volunteer_cheer_create]
+ before_action :set_info, only: [:volunteer_cheer_create, :volunteer_cheer_destroy]
 
  def volunteer_cheer_index
  end
 
  def volunteer_cheer_create
-   set_info
    cheer = Cheer.new(cheerable_id: @account_id, cheerable_type:@account_type, targetable_id:@target_id, targetable_type:@target_type)
    if cheer.save!
      @volunteer.create_notification_cheer_registration!(@volunteer, @account_info)
      redirect_to volunteer_path(@volunteer.id)
    end
+ end
+
+ def volunteer_cheer_destroy
+  cheer = Cheer.find_by(cheerable_id: @account_id, cheerable_type: @account_type, targetable_id:params[:volunteer_id])
+  if cheer.destroy
+    redirect_to volunteer_path(params[:volunteer_id])
+  end
  end
  
  def move_to_index
