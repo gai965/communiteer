85cfdc50752530f5f6ed9ac79bf09dd88d984891
gai965 <kotodama51080@gmail.com>
2021-05-26 23:42:50 +0900

    エラー表示

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -22,13 +22,13 @@ RSpec.describe Group, type: :model do
       it '名前が「空」だと登録できない' do
         @group.name = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include("Name can't be blank")
+        expect(@group.errors.full_messages).to include('名前を入力してください')
       end
 
       it 'メールアドレスが「空」だと登録できない' do
         @group.email = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include("Email can't be blank")
+        expect(@group.errors.full_messages).to include('メールアドレスを入力してください')
       end
 
       it 'メールアドレスが「一意性でない」と登録できない' do
@@ -36,129 +36,129 @@ RSpec.describe Group, type: :model do
         another_group = FactoryBot.build(:group)
         another_group.email = @group.email
         another_group.valid?
-        expect(another_group.errors.full_messages).to include('Email has already been taken')
+        expect(another_group.errors.full_messages).to include('メールアドレスはすでに存在します')
       end
 
       it 'メールアドレスは「@を含まない」と登録できない' do
         @group.email = 'test.test'
         @group.valid?
-        expect(@group.errors.full_messages).to include('Email is invalid')
+        expect(@group.errors.full_messages).to include('メールアドレスは不正な値です')
       end
 
       it '電話番号は「空」だと登録できない' do
         @group.phone_number = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include("Phone number can't be blank")
+        expect(@group.errors.full_messages).to include('電話番号を入力してください')
       end
 
       it '電話番号は「9桁以下」だと登録できない' do
         @group.phone_number = '123456789'
         @group.valid?
-        expect(@group.errors.full_messages).to include('Phone number Applies to character restrictions')
+        expect(@group.errors.full_messages).to include('電話番号文字制限に適用されます')
       end
 
       it '電話番号は「12桁以上」だと登録できない' do
         @group.phone_number = '012345678901'
         @group.valid?
-        expect(@group.errors.full_messages).to include('Phone number Applies to character restrictions')
+        expect(@group.errors.full_messages).to include('電話番号文字制限に適用されます')
       end
 
       it '電話番号は「全角数字」だと登録できない' do
         @group.phone_number = '１２３４５６７８９'
         @group.valid?
-        expect(@group.errors.full_messages).to include('Phone number Applies to character restrictions')
+        expect(@group.errors.full_messages).to include('電話番号文字制限に適用されます')
       end
 
       it '住所が「空」だと登録できない' do
         @group.base_address = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include("Base address can't be blank")
+        expect(@group.errors.full_messages).to include('住所を入力してください')
       end
 
-      it 'HPアドレスに「"http"または"https"が含まれない」と登録できない' do
+      it 'HPアドレスに「httpまたはhttpsが含まれない」と登録できない' do
         @group.url = 'htp'
         @group.valid?
-        expect(@group.errors.full_messages).to include('Url Include "http" or "https" in the acronym')
+        expect(@group.errors.full_messages).to include('ホームページアドレスの頭字語に「http」または「https」を含めてください')
       end
 
       it 'カテゴリーが「空」だと登録できない' do
         @group.group_category = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include('Group category Select')
+        expect(@group.errors.full_messages).to include('カテゴリーを選択してください')
       end
 
       it 'パスワードが「空」だと登録できない' do
         @group.password = nil
         @group.valid?
-        expect(@group.errors.full_messages).to include("Password can't be blank")
+        expect(@group.errors.full_messages).to include('パスワードを入力してください')
       end
 
       it 'パスワードは「6文字以下」だと登録できない' do
         @group.password = '123Ab'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+        expect(@group.errors.full_messages).to include('パスワードは6文字以上で入力してください')
       end
 
       it 'パスワードは「半角数字のみ」だと登録できない' do
         @group.password = '123456'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「半角英字のみ」だと登録できない' do
         @group.password = 'abcABC'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「半角英小文字のみ」だと登録できない' do
         @group.password = 'abcdef'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「半角英大文字のみ」だと登録できない' do
         @group.password = 'ABCDEF'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「半角英数字の小文字のみ」だと登録できない' do
         @group.password = '123abc'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「半角英数字の大文字のみ」だと登録できない' do
         @group.password = '123ABC'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「全角英文字が含まれる」と登録できない' do
         @group.password = '1234Ａｂ'
         @group.password_confirmation = @group.password
         @group.valid?
-        expect(@group.errors.full_messages).to include('Password Include all uppercase and lowercase letters and numbers')
+        expect(@group.errors.full_messages).to include('パスワードは大文字を含む英字と数字を入力してください')
       end
 
       it 'パスワードは「確認用を含めて2回入力しない」と登録できない' do
         @group.password_confirmation = ''
         @group.valid?
-        expect(@group.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@group.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
       end
 
       it 'パスワードとパスワード（確認用）の「値の一致しない」と登録できない' do
         @group.password_confirmation = FactoryBot.build(:group).password
         @group.valid?
-        expect(@group.errors.full_messages).to include("Password confirmation doesn't match Password")
+        expect(@group.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
       end
     end
   end
