934f5a174c02dbcdd4889e4012e7779a7cd18e7f
gai965 <kotodama51080@gmail.com>
2021-06-17 21:26:25 +0900

    遷移ページから元ページへの遷移設定2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,18 +9,5 @@ module ApplicationHelper
     end
   end
 
-  def return_path_get
-    path = Rails.application.routes.recognize_path(request.referer)
-    # binding.pry
-    if path[:controller] == 'notifications'
-      return_path = notifications_path
-    elsif path[:controller] == 'volunteers'
-      return_path = params[:page]
-    elsif path[:controller] == 'join_volunteers'|| path[:controller] == 'accepts'
-      return_path = volunteer_join_index_path(params[:volunteer_id])
-    else
-      return_path = root_path
-    end
-    return_path
-  end
+  
 end
--- app/helpers/join_volunteers_helper.rb
+++ app/helpers/join_volunteers_helper.rb
@@ -1,2 +1,5 @@
 module JoinVolunteersHelper
+  def return_joinvolunteer_path
+    volunteer_path(@volunteer.id, page: params[:second_pre_page])
+  end
 end
