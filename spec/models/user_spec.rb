cb0a07c5b0fc2be41bb9bd48a3d583da6d9b5bc2
gai965 <kotodama51080@gmail.com>
2021-07-29 18:10:24 +0900

    CircleCIの実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,6 @@ RSpec.describe User, type: :model do
         expect(another_user.errors.full_messages).to include('ニックネームはすでに存在します')
       end
 
-
       it 'メールアドレスが「空」だと登録できない' do
         @user.email = nil
         @user.valid?
