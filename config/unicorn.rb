ff745ce8347795fd658ec25d6d3b0cfaab5860c5
gai965 <kotodama51080@gmail.com>
2021-08-27 00:20:08 +0900

    unicornの設定

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,31 +1,30 @@
+# rootパスのディレクトリを指定
+root_path = File.expand_path('../../', __FILE__)
 
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
-app_path = File.expand_path('../../../', __FILE__)  # 「../」が一つ増えている
+# アプリケーションサーバの性能を決定する
+worker_processes 2
 
-#アプリケーションサーバの性能を決定する
-worker_processes 1
+# アプリケーションの設置されているディレクトリを指定
+working_directory root_path
 
-#アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"  # 「current」を指定
+# プロセスIDの保存先を指定
+pid "#{root_path}/tmp/pids/unicorn.pid"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
+# ポート番号を指定
+listen "#{root_path}/tmp/sockets/unicorn.sock"
 
-#ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"  # 「shared」の中を参照するよう変更
+# エラーのログを記録するファイルを指定
+stderr_path "#{root_path}/log/unicorn.stderr.log"
 
-#エラーのログを記録するファイルを指定
-stderr_path "#{app_path}/shared/log/unicorn.stderr.log"  # 「shared」の中を参照するよう変更
+# 通常のログを記録するファイルを指定
+stdout_path "#{root_path}/log/unicorn.stdout.log"
 
-#通常のログを記録するファイルを指定
-stdout_path "#{app_path}/shared/log/unicorn.stdout.log"  # 「shared」の中を参照するよう変更
+#応答時間を待つ上限時間を設定
+timeout 30
 
-#Railsアプリケーションの応答を待つ上限時間を設定
-timeout 60
-
-#以下は応用的な設定なので説明は割愛
-
+# ダウンタイムなしでUnicornを再起動時する
 preload_app true
+
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
 check_client_connection false
