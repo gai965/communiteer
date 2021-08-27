2f0d713b745317e1f9b94eb48d5c70a7e7264461
gai965 <kotodama51080@gmail.com>
2021-08-28 01:04:10 +0900

    Setup puma

--- config/puma.rb
+++ config/puma.rb
@@ -14,8 +14,7 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,3 +40,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
