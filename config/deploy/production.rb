fed4514c1451ce4cf33bb49a727d8242f1e91447
gai965 <kotodama51080@gmail.com>
2021-08-31 19:47:19 +0900

    communiteer_key_rsaに変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 server "54.249.174.89", user: "ec2-user", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/communiteer-app.pem),
+  keys: %w(~/.ssh/communiteer_key_rsa),
   forward_agent: true,
   auth_methods: %w(publickey),
 }
