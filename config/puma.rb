b3c77f7777fd39432a43f61af6f513a8fd0e971d
gai965 <kotodama51080@gmail.com>
2021-09-11 18:00:08 +0900

    Setup puma for production

--- config/puma.rb
+++ config/puma.rb
@@ -21,7 +21,7 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -41,4 +41,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-# bind "unix:/var/www/rails/communiteer/tmp/sockets/puma.sock"
+bind "unix:/var/www/rails/communiteer/tmp/sockets/puma.sock"
