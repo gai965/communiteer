3bb8d77b0f78f0133ad0acf50759e470b19a6f28
gai965 <kotodama51080@gmail.com>
2021-09-12 17:10:27 +0900

    depoyの情報記載

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -40,4 +40,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
 
-# bind "unix:/var/www/rails/communiteer/tmp/sockets/puma.sock"
+bind "unix:/var/www/rails/communiteer/tmp/sockets/puma.sock"
