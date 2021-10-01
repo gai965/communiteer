3138c07ce1fae65195508b1487aec53133f258e7
gai965 <kotodama51080@gmail.com>
2021-10-02 05:29:41 +0900

    pumaの自動起動設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,19 @@
 
-lock "~> 3.16.0"
+lock '~> 3.16.0'
 
-set :application, "communiteer"
-set :repo_url, "https://github.com/gai965/communiteer.git"
+set :application, 'communiteer'
+set :repo_url, 'https://github.com/gai965/communiteer.git'
 
 set :rbenv_ruby, File.read('.ruby-version').strip
-set :branch, ENV['BRANCH'] || "master"
+set :branch, ENV['BRANCH'] || 'master'
 
 # Nginxの設定ファイル名と置き場所を修正
 set :nginx_config_name, "#{fetch(:application)}.conf"
-set :nginx_sites_enabled_path, "/etc/nginx/conf.d"
+set :nginx_sites_enabled_path, '/etc/nginx/conf.d'
 
-# append :linked_files, "config/shared/master.key"
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "node_modules"
+# append :linked_files, 'config/shared/master.key'
+set :linked_files, fetch(:linked_files, []).push('config/master.key')
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules'
 
 
 namespace :deploy do
@@ -22,7 +22,7 @@ namespace :deploy do
     on roles(:db) do
       with rails_env: fetch(:rails_env) do
         within current_path do
-          execute :rake, "db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
+          execute :rake, 'db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
           execute :rake, 'db:seed'
         end
       end
@@ -31,3 +31,16 @@ namespace :deploy do
 end
 
 after 'deploy:assets:backup_manifest', 'deploy:migrate_seed'
+
+namespace :deploy do
+  task :restart do
+    on roles(:web), in: :sequence, wait: 5 do
+      within release_path do
+        execute 'sudo systemctl daemon-reload'
+        execute 'sudo systemctl restart puma'
+      end
+    end
+  end
+
+  after :finishing, :restart
+end
