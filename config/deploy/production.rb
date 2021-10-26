8b714c79a9e2e935ffa1ae7639964d52a58d0fe6
gai865 <Kotodama51080@gmail.com>
2021-10-26 12:33:19 +0900

    デプロイ先の変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 server "54.249.174.89", user: "ec2-user", roles: %w{app db web}
-set :deploy_to, '/var/www/rails/communiteer'
+set :deploy_to, '/var/www/communiteer'
 
 set :ssh_options, {
-  keys: %w(~/.ssh/communiteer_key_rsa),
+  keys: %w(~/.ssh/communiteer-app.pem),
   forward_agent: true,
   auth_methods: %w(publickey),
 }
