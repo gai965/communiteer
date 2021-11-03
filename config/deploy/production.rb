e86fdc138899919b56e15bc1ee3fb81eef1beb71
gai965 <Kotodama51080@gmail.com>
2021-11-03 20:13:29 +0900

    updata production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,19 @@
-server "54.249.174.89", user: "ec2-user", roles: %w{app db web}
-set :deploy_to, '/var/www/communiteer'
+# server "54.249.174.89", user: "ec2-user", roles: %w{app db web}
+# set :deploy_to, '/var/www/communiteer'
 
-set :ssh_options, {
-  keys: %w(~/.ssh/communiteer-app.pem),
-  forward_agent: true,
-  auth_methods: %w(publickey),
-}
+# set :ssh_options, {
+#   keys: %w(~/.ssh/communiteer-app.pem),
+#   forward_agent: true,
+#   auth_methods: %w(publickey),
+# }
+
+server '54.249.174.89',
+   user: "ec2-user",
+   roles: %w{web db app},
+   set :deploy_to, '/var/www/communiteer',
+   ssh_options: {
+      user: "ec2-user",
+      keys: %w(~/.ssh/communiteer-app.pem),
+      forward_agent: true
+      auth_methods: %w(publickey password)
+  } 
