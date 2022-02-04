d39acb0ad5278a04889268ac8e01f64ff17ec6cd
gai965 <Kotodama51080@gmail.com>
2022-02-04 23:42:16 +0900

    deploy.rbの変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,18 +15,18 @@ set :linked_files, fetch(:linked_files, []).push('config/master.key')
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'node_modules'
 
 
-# namespace :deploy do
-#   desc 'Run rake db:migrate:reset & rake db:seed'
-#   task :migrate_seed do 
-#     on roles(:db) do
-#       with rails_env: fetch(:rails_env) do
-#         within current_path do
-#           execute :rake, 'db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
-#           execute :rake, 'db:seed'
-#         end
-#       end
-#     end
-#   end
-# end
+namespace :deploy do
+  desc 'Run rake db:migrate:reset & rake db:seed'
+  task :migrate_seed do 
+    on roles(:db) do
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :rake, 'db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1'
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+end
 
-# after 'deploy:assets:backup_manifest', 'deploy:migrate_seed'
+after 'deploy:assets:backup_manifest', 'deploy:migrate_seed'
